29b37ff2b136b1c295f4e75f963148f2e70e0229
bpbbpbtest <bpbbpbtest@gmail.com>
2026-08-23 07:36:31 +0330

    Update add_configs.sh

--- scripts/add_configs.sh
+++ scripts/add_configs.sh
@@ -5,7 +5,7 @@ TOKEN="${MAHSA_API_TOKEN:?MAHSA_API_TOKEN secret is not set}"
 API_URL="${MAHSA_API_URL:-https://www.mahsaserver.com/backend/api/v1/config/}"
 CONFIGS_FILE="${CONFIGS_FILE:-config.txt}"
 DELAY="${DELAY:-30}"
-ADS_URL="${ADS_URL:- برای کانفیگ های بیشتر به تلگرام ما بپیوندید     https://t.me/DeltaKroneckerGithub}"
+ADS_URL="${ADS_URL:-🔥برای اتصال رایگان و با کیفیت آموزش های پین شده را چک کنید: https://t.me/DeltaKroneckerGithub}"
 USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0 Safari/537.36"
 MAX_ATTEMPTS=15
 
